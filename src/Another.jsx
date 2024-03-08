70a1c8a9e2395765dde125a035e9212a88dda63e
Christian Valeri <mortlaxris@outlook.com>
2024-03-07 21:11:35 -0600

    added another jsx page

--- src/Another.jsx
+++ src/Another.jsx
@@ -1,18 +1,24 @@
 import { useState } from 'react';
 import './css/Another.css'
+import Navbar from './navbar.jsx';
 
 function Another() {
-    let [tally, setTally] = useState(0);
+    let [tally, setTally] = useState(100);
 
     function updateTally() {
-        setTally((tally) => tally + 1);
+        if (tally > 0) {
+            setTally((tally) => tally - 1);
+        }
     }
 
     return (
         <>
-            <h1>Hello this is another page</h1>
-            <h2>{tally}</h2>
-            <button onClick={updateTally}>Click to update tally</button>
+            <Navbar />
+            <div>
+                <h1>Hello this is another page</h1>
+                <h2>{tally}</h2>
+                <button onClick={updateTally}>Click to update tally</button>
+            </div>
         </>
     )
 }
