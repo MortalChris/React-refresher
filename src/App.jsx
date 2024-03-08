442a23f198fa1283dd0db094ae40fef81c7c0e5c
Christian Valeri <mortlaxris@outlook.com>
2024-03-07 21:11:59 -0600

    added navbar and new elements in app function

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 // import { useState } from 'react'
 import New from './New.jsx'
+import Navbar from './navbar.jsx';
 import './css/App.css'
 
 function App() {
-
-
   return (
     <>
+      <Navbar />
       <New />
     </>
   )
