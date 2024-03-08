d5d936d57af8872ac287b0b4f6b79ba4c128beec
Christian Valeri <mortlaxris@outlook.com>
2024-03-07 21:12:14 -0600

    made a navbar element function

---
+++ src/navbar.jsx
@@ -0,0 +1,18 @@
+
+function NavBar() {
+    return (
+        <>
+            <ul id="navBar">
+                <div id="leftNav">
+                    <img src="" alt="" />
+                    <button><a href="/">Homepage</a></button>
+                </div>
+                <div id="rightNav">
+                    <button><a href="/Another">Another Page</a></button>
+                </div>
+            </ul>
+        </>
+    )
+}
+
+export default NavBar;
