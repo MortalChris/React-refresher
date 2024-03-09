a6b8577a91a2b73c91cd567cd8e089bf5a2acb28
Christian Valeri <mortlaxris@outlook.com>
2024-03-08 22:41:18 -0600

    added element array and mapped it out into li

--- src/Another.jsx
+++ src/Another.jsx
@@ -5,6 +5,8 @@ import Navbar from './navbar.jsx';
 function Another() {
     let [tally, setTally] = useState(100);
 
+    let elements = ['one', 'two', 'three', 'four', 'five', 'six']
+
     function updateTally() {
         if (tally > 0) {
             setTally((tally) => tally - 1);
@@ -14,8 +16,12 @@ function Another() {
     return (
         <>
             <Navbar />
+            {/* <PropExample name="UniqueClassName" idName="UniqueId" /> */}
             <div>
                 <h1>Hello this is another page</h1>
+                {elements.map(function(value, index) {
+                    return <li key={index}>{value}</li>
+                })}
                 <h2>{tally}</h2>
                 <button onClick={updateTally}>Click to update tally</button>
             </div>
@@ -23,4 +29,14 @@ function Another() {
     )
 }
 
+// function PropExample({ name, idName }) {
+//     return (
+//         <>
+//             <ol>
+//                 <li>{name}</li>
+//                 <li>{idName}</li>
+//             </ol>
+//         </>
+//     )
+// }
 export default Another
